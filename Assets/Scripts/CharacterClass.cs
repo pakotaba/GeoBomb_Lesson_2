d3dc412eee5a87fbc7cf3b50735eeb5c622fa987
pakotaba <tabatadzepavle@gmail.com>
2016-05-10 19:30:24 +0400

    Project Birth

---
+++ Assets/Scripts/CharacterClass.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterClass : MonoBehaviour {
+    public float Speed = 0.25f;
+	void Start () {
+
+    }
+    
+    void ChoosePosition(string dir)
+    {
+        switch (dir)
+        {
+            case "left":
+                gameObject.transform.Translate(-Speed, 0, 0);
+                break;
+            case "right":
+                gameObject.transform.Translate(Speed, 0, 0);
+                break;
+            case "up":
+                gameObject.transform.Translate(0, Speed, 0);
+                break;
+            case "down":
+                gameObject.transform.Translate(0, -Speed, 0);
+                break;
+        }
+    }
+
+	void Update ()
+    {
+         if (Input.GetKey(KeyCode.A) && gameObject.transform.position.x > -5)
+            ChoosePosition("left");
+         if (Input.GetKey(KeyCode.D) && gameObject.transform.position.x < 5)
+            ChoosePosition("right");
+        if (Input.GetKey(KeyCode.W) && gameObject.transform.position.y < 5)
+            ChoosePosition("up");
+        if (Input.GetKey(KeyCode.S) && gameObject.transform.position.y > -5)
+            ChoosePosition("down");
+
+    }
+}
