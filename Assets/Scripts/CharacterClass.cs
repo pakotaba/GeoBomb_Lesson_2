bc6a2ce14191cdeb7408937acfd1ad7ffe5e445c
pakotaba <tabatadzepavle@gmail.com>
2016-05-23 00:38:46 +0400

    Wall Generator, Fixed Animations

--- Assets/Scripts/CharacterClass.cs
+++ Assets/Scripts/CharacterClass.cs
@@ -2,54 +2,75 @@
 using System.Collections;
 
 public class CharacterClass : MonoBehaviour {
-    public float Speed = 0.25f;
-    Animator anim;
-	void Start () {
-        anim = gameObject.GetComponent<Animator>();
+
+    public float Speed = 0.2f;
+    Animator Anim;
+
+	void Start () 
+	{
+        Anim = gameObject.GetComponent<Animator>();
+
     }
-    
-    void ChoosePosition(string dir)
+    void ChangePosition( string dir)
     {
-        switch (dir)
+        switch(dir)
         {
-            case "left":
+		case "left":
                 gameObject.transform.Translate(-Speed, 0, 0);
-
-                if(gameObject.transform.localScale.x > 0)
-                  gameObject.transform.localScale = new Vector3(-gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
-
-                anim.SetTrigger("MoveSide");
                 break;
             case "right":
                 gameObject.transform.Translate(Speed, 0, 0);
-                if (gameObject.transform.localScale.x < 0)
-                    gameObject.transform.localScale = new Vector3(-gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
-
-                anim.SetTrigger("MoveSide");
                 break;
             case "up":
                 gameObject.transform.Translate(0, Speed, 0);
                 break;
             case "down":
                 gameObject.transform.Translate(0, -Speed, 0);
-                anim.SetTrigger("MoveDown");
                 break;
         }
     }
 
-   
-
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.A) && gameObject.transform.position.x > -5)
-            ChoosePosition("left");
-        if (Input.GetKey(KeyCode.D) && gameObject.transform.position.x < 5)
-            ChoosePosition("right");
-        if (Input.GetKey(KeyCode.W) && gameObject.transform.position.y < 5)
-            ChoosePosition("up");
-        if (Input.GetKey(KeyCode.S) && gameObject.transform.position.y > -5)
-            ChoosePosition("down");
+        if (Input.GetKey(KeyCode.A) && gameObject.transform.position.x > -4)
+            ChangePosition("left");
+        if (Input.GetKey(KeyCode.D) && gameObject.transform.position.x < 4)
+            ChangePosition("right");
+        if (Input.GetKey(KeyCode.W) && gameObject.transform.position.y < 3.1f)
+            ChangePosition("up");
+        if (Input.GetKey(KeyCode.S) && gameObject.transform.position.y > -3.1f)
+            ChangePosition("down");
+
+		if (Input.GetKeyDown (KeyCode.A) && gameObject.transform.position.x > -4) 
+		{
+			if (gameObject.transform.localScale.x > 0)
+				gameObject.transform.localScale = new Vector3 (-gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+			Anim.SetTrigger ("MoveSide");
+		}
+
+		if (Input.GetKeyDown (KeyCode.D) && gameObject.transform.position.x < 4)
+		{
+			if (gameObject.transform.localScale.x < 0)
+				gameObject.transform.localScale = new Vector3 (-gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+			Anim.SetTrigger ("MoveSide");
+		}
+		if (Input.GetKeyDown (KeyCode.W) && gameObject.transform.position.y < 3.1f) 
+		{
+			Anim.SetTrigger ("MoveBack");
+		}
+		if (Input.GetKeyDown (KeyCode.S) && gameObject.transform.position.y > -3.1f) 
+		{
+			Anim.SetTrigger ("MoveFront");
+		}
+
+       if (Input.GetKeyUp(KeyCode.A) && gameObject.transform.position.x > -4)
+            Anim.SetTrigger("Stop");
+        if (Input.GetKeyUp(KeyCode.D) && gameObject.transform.position.x < 4)
+            Anim.SetTrigger("Stop");
+        if (Input.GetKeyUp(KeyCode.W) && gameObject.transform.position.y < 3.1f)
+            Anim.SetTrigger("Stop");
+        if (Input.GetKeyUp(KeyCode.S) && gameObject.transform.position.y > -3.1f)
+            Anim.SetTrigger("Stop");
+
     }
-
-
 }
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+    public GameObject _bg;
+    public GameObject _borderBg;
+    public float xStart = -4;
+    public float yStart = 3;
+    public float D = 0.6f;
+    float X;
+    int xBlock = 15;
+    int yBlock = 10;
+
+
+    void Start ()
+    {
+        X = xStart;
+        GenerateBG();
+    }
+    void GenerateBG()
+    {
+        for(int k = 0; k<yBlock; k++)
+        { 
+        for(int i = 0; i<xBlock; i++)
+        { if(i==0 || k==0)
+           Instantiate(_borderBg, new Vector3(xStart, yStart, 0), Quaternion.identity);
+          else if (i == xBlock-1 || k == yBlock-1)
+                    Instantiate(_borderBg, new Vector3(xStart, yStart, 0), Quaternion.identity);
+            else
+                    Instantiate(_bg, new Vector3(xStart, yStart, 0), Quaternion.identity);
+
+                xStart += D;
+        }
+            xStart = X;
+            yStart -= D;
+        }
+
+    }
+	void Update () {
+	
+	}
+}
